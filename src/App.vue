3a21a00eff1c3497981ace4274a996094ead1b2a
Patrick Roza <contact@patrickroza.com>
2024-02-26 21:42:45 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import * as S from "@effect/schema/Schema"
+
+const s = S.struct({ hello: S.string, world: S.number })
+const v = S.decodeSync(s)({ hello: "world", world: 42 })
+console.log(v)
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
